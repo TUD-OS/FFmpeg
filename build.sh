527e74706d1346acbde3950c109de1b8c13d578b
Georg Grassnick <georg.grassnick@mailbox.tu-dresden.de>
2018-10-27 15:20:29 +0200

    Add debugging symbols to decoder frontend

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 make -j9
 
 # build decoder frontend
-gcc -O3 -I. hevc_decode.c -DHAVE_INLINE_ASM ./libavcodec/libavcodec.a -lavformat -lavdevice ./libavutil/libavutil.a $(pkg-config --libs --static libavcodec)  -o hevc_decode
+gcc -O3 -ggdb -I. hevc_decode.c -DHAVE_INLINE_ASM ./libavcodec/libavcodec.a -lavformat -lavdevice ./libavutil/libavutil.a $(pkg-config --libs --static libavcodec)  -o hevc_decode
